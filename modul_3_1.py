c69cabcf8ad03107bece0a3057155d7bd2314541
Igor4084 <ovsyannikov.23.io@gmail.com>
2024-11-06 11:30:46 +0300

    Add files via upload

--- modul_3_1.py
+++ modul_3_1.py
@@ -1,8 +1,9 @@
 # счетчик вызовов
 calls = 0
 def count_calls():
-     global calls
-     calls = calls + 1
+    global calls
+    calls = calls + 1
+    return calls
 def strig_info(string):
     count_calls()
     a = len(string)
@@ -20,6 +21,8 @@ def is_contains(string, list_to_search):
         return True
     else:
         return False
+
 print(is_contains(string='Urban', list_to_search=['banan', 'urban', 'ban']))
 print(is_contains(string='Arba', list_to_search=['banan', 'urban', 'ban']))
+
 print(calls)
