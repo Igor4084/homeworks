788c1b89c5b9ffed362c5ca8c60e3e691a06f80d
Igor4084 <ovsyannikov.23.io@gmail.com>
2024-11-06 11:29:28 +0300

    Update modul_3_1.py

--- modul_3_1.py
+++ modul_3_1.py
@@ -1,29 +1,25 @@
 # счетчик вызовов
 calls = 0
 def count_calls():
-    def strig_info(string):
-        global calls
-        calls = calls + 1
-        a = len(string)
-        b = string.upper()
-        c = string.lower()
-        return (a, b, c)
+     global calls
+     calls = calls + 1
+def strig_info(string):
+    count_calls()
+    a = len(string)
+    b = string.upper()
+    c = string.lower()
+    return (a, b, c)
 
-    print(strig_info(string ='bananas'))
-    print(strig_info(string='Capybara'))
+print(strig_info(string ='bananas'))
+print(strig_info(string='Capybara'))
 
-    def is_contains(string, list_to_search):
-        global calls
-        calls = calls + 1
-        new_list = [a.lower() for a in list_to_search]
-        if string.lower() in new_list:
-            return True
-        else:
-            return False
-
-    print(is_contains(string='Urban', list_to_search=['banan', 'urban', 'ban']))
-    print(is_contains(string='Arba', list_to_search=['banan', 'urban', 'ban']))
-    return calls
-
-
-print(count_calls())
+def is_contains(string, list_to_search):
+    count_calls()
+    new_list = [a.lower() for a in list_to_search]
+    if string.lower() in new_list:
+        return True
+    else:
+        return False
+print(is_contains(string='Urban', list_to_search=['banan', 'urban', 'ban']))
+print(is_contains(string='Arba', list_to_search=['banan', 'urban', 'ban']))
+print(calls)
